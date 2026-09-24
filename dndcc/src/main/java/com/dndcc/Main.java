df6306d042d5c8d23e371259ea63b96805ca20d5
Leo Alkberg <leo@lalkberg.com>
2026-09-24 12:36:44 +0200

    Refactor task columns and add initial Maven project structure with main class

---
+++ dndcc/src/main/java/com/dndcc/Main.java
@@ -0,0 +1,7 @@
+package com.dndcc;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome to DNDCC! What type of D&D character would you like to create?");
+    }
+}
